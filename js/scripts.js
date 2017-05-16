051ca5fc6bffece59689693c3a0238f6e88b8370
kmolema@gmail.com <kmolema@gmail.com>
2017-05-17 00:19:31 +0200

    Initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 window.setInterval(function() {reloadFrame()},60000);
 function reloadFrame() {
 		//document.frames["reggraph"].location.reload();
-		document.getElementById('reggraph').contentWindow.location = "../res/tasgraphregproj.php";
-		document.getElementById('clsgraph').contentWindow.location = "../res/tasgraphclsproj.html";
-		document.getElementById('opengraph').contentWindow.location = "../res/tasgraphopenproj.html";
+		document.getElementById('reggraph').contentWindow.location = "./tasgraphregproj.php";
+		document.getElementById('clsgraph').contentWindow.location = "./tasgraphclsproj.html";
+		document.getElementById('opengraph').contentWindow.location = "./tasgraphopenproj.html";
 	}
---
+++ pages/prgdetails.php
@@ -0,0 +1,35 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Program Details</title>
+<meta name="generator" content="WYSIWYG Web Builder 9 - http://www.wysiwygwebbuilder.com">
+<link rel="stylesheet" href="../styles/style.css">
+</head>
+<body style="text-align:center">
+<div class="banner" alt="Program Details" style="width:100%;height:40px;z-index:12;postion:relative">
+<div id="wb_Form1" style="width:772px;height:267px;z-index:13;">
+<form name="PrgDetailsForm" method="post" action="" enctype="text/plain" id="Form1">
+<div id="wb_Text1" style="position:absolute;left:10px;top:15px;width:148px;height:16px;z-index:0;text-align:left;">
+<span style="color:#000000;font-family:Arial;font-size:13px;">Financial Year:</span></div>
+<input type="text" id="Editbox1" style="position:absolute;left:168px;top:15px;width:198px;height:23px;line-height:23px;z-index:1;" name="Editbox1" value="2017/2018" readonly="readonly">
+<div id="wb_Text2" style="position:absolute;left:10px;top:45px;width:148px;height:16px;z-index:2;text-align:left;">
+<span style="color:#000000;font-family:Arial;font-size:13px;">Budget:</span></div>
+<input type="text" id="Editbox2" style="position:absolute;left:168px;top:45px;width:198px;height:23px;line-height:23px;z-index:3;" name="Editbox2" value="R19,000,000.00" readonly="readonly">
+<div id="wb_Text3" style="position:absolute;left:10px;top:75px;width:122px;height:24px;z-index:4;text-align:left;">
+<h4>Targets:</h4></div>
+<div id="wb_Text4" style="position:absolute;left:10px;top:100px;width:148px;height:16px;z-index:5;text-align:left;">
+<span style="color:#000000;font-family:Arial;font-size:13px;">No of projects:</span></div>
+<input type="text" id="Editbox3" style="position:absolute;left:168px;top:100px;width:198px;height:23px;line-height:23px;z-index:6;" name="Editbox3" value="">
+<div id="wb_Text5" style="position:absolute;left:10px;top:130px;width:148px;height:16px;z-index:7;text-align:left;">
+<span style="color:#000000;font-family:Arial;font-size:13px;">No of jobs:</span></div>
+<input type="text" id="Editbox4" style="position:absolute;left:168px;top:130px;width:198px;height:23px;line-height:23px;z-index:8;" name="Editbox4" value="">
+<div id="wb_Text6" style="position:absolute;left:10px;top:160px;width:148px;height:16px;z-index:9;text-align:left;">
+<span style="color:#000000;font-family:Arial;font-size:13px;">Average jobs per project:</span></div>
+<input type="text" id="Editbox5" style="position:absolute;left:168px;top:160px;width:198px;height:23px;line-height:23px;z-index:10;" name="Editbox5" value="">
+<input type="reset" id="Button1" name="" value="Reset" style="position:absolute;left:63px;top:206px;width:96px;height:25px;z-index:11;">
+</form>
+</div>
+<input type="submit" id="Button2" name="" value="Submit" style="position:absolute;left:212px;top:258px;width:96px;height:25px;z-index:14;">
+</body>
+</html>
