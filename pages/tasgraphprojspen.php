b4264e20631157a6c039f5e1770301377bc2445c
kmolema@gmail.com <kmolema@gmail.com>
2017-05-24 02:26:28 +0200

    Added No of projects graph

---
+++ pages/tasgraphprojspen.php
@@ -0,0 +1,120 @@
+<?php
+$servername = "localhost";
+$username = "gateway1_tasuser";
+$password = "tasuser123";
+$dbname = "gateway1_tas";
+$mysql_table = "STATS";
+
+$conn = new mysqli($servername, $username, $password, '');
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error."<br>");
+} 
+if (!$conn->select_db($dbname)) {
+	die( "Error: Failed to select database '$dbname' ".$conn->error."<br>");
+}
+
+$sql = "SELECT 'ACTUAL' AS 'DESC', ACTUAL 'VALUE', DESCRIPTION 'STAT' FROM STATS WHERE STAT_TYPE = 'ProjSpen' ";
+$sql .= "UNION "; 
+$sql .= "SELECT 'BALANCE' AS 'DESC', BALANCE 'VALUE', DESCRIPTION 'STAT' FROM STATS WHERE STAT_TYPE = 'ProjSpen'";
+
+?>
+<html>
+	<head>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.1.4/Chart.min.js"></script>
+	</head>
+	<body>
+		<h5 style="text-align: center;">No. of Projects</h5>
+		<canvas id="myChart" width="200px" height="100px"></canvas>
+		<script>
+			var ctx = document.getElementById("myChart").getContext('2d');
+			var myChart = new Chart(ctx, {
+				type: 'doughnut',
+				data: {
+
+<?php
+$labels = "labels: [";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+	$rowsremaining = $result->num_rows;
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        //echo $row["PROVINCE"]. " | " . $row["REGISTERED"]. " | " . $row["OPEN"]. " | " . $row["CLOSED"]. " | " . "<br>";
+		$labels .= "'".$row["DESC"]."'";
+		$rowsremaining--;
+		if ($rowsremaining > 0) {
+			$labels .= ",";
+		}
+		//echo "Num_rows: ".$rowsremaining."<br>";
+    }
+	$labels .= "],";
+} else {
+    echo "0 results";
+}
+//echo "<br><br>";
+
+
+echo $labels;
+?>
+					datasets: [{
+						label: 'No. of Projects',
+						
+<?php
+
+						//data: [8,21,7,12,4,27],
+$data = "data: [";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+	$rowsremaining = $result->num_rows;
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        //echo $row["PROVINCE"]. " | " . $row["REGISTERED"]. " | " . $row["OPEN"]. " | " . $row["CLOSED"]. " | " . "<br>";
+		$data .= $row["VALUE"];
+		$rowsremaining--;
+		if ($rowsremaining > 0) {
+			$data .= ",";
+		}
+		//echo "Num_rows: ".$rowsremaining."<br>";
+    }
+	$data .= "],";
+} else {
+    echo "0 results";
+}
+echo $data;
+
+?>						
+						
+						backgroundColor: [
+							'rgba(255, 99, 132, 0.2)',
+							'rgba(54, 162, 235, 0.2)',
+							'rgba(255, 206, 86, 0.2)',
+							'rgba(75, 192, 192, 0.2)',
+							'rgba(153, 102, 255, 0.2)',
+							'rgba(255, 159, 64, 0.2)'
+						],
+						borderColor: [
+							'rgba(255,99,132,1)',
+							'rgba(54, 162, 235, 1)',
+							'rgba(255, 206, 86, 1)',
+							'rgba(75, 192, 192, 1)',
+							'rgba(153, 102, 255, 1)',
+							'rgba(255, 159, 64, 1)'
+						],
+						borderWidth: 1
+					}]
+				},
+				options: {
+					scales: {
+						yAxes: [{
+							ticks: {
+								beginAtZero:true
+							}
+						}]
+					}
+				}
+			});
+		</script>
+	</body>
+</html>
