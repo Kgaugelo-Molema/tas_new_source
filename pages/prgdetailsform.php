09b6df3bfde016b31333383599db31278e7fe285
kmolema@gmail.com <kmolema@gmail.com>
2017-05-27 21:27:27 +0200

    TAS.NEW 0.02.002: Created Program details capturing form.

---
+++ pages/prgdetailsform.php
@@ -0,0 +1,99 @@
+<?php
+$servername = "localhost";
+$username = "gateway1_tasuser";
+$password = "tasuser123";
+$dbname = "gateway1_tas";
+$mysql_table = "PROGRAMS";
+
+$conn = new mysqli($servername, $username, $password, '');
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error."<br>");
+} 
+if (!$conn->select_db($dbname)) {
+	die( "Error: Failed to select database '$dbname' ".$conn->error."<br>");
+}
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $sqltext = "INSERT INTO $mysql_table (`DATESTAMP`, `TIME`, `IP`, `BROWSER`, `STAT_TYPE`, `YEAR`, `QUARTER`, `PROVINCE`, `QTY`, `QTY_PCT`, `BUDGET`)
+                       VALUES ('".date("Y-m-d")."',
+                       '".date("G:i:s")."',
+                       '".$_SERVER['REMOTE_ADDR']."',
+                       '".$_SERVER['HTTP_USER_AGENT']."'";
+    $inputvalues = "'".$_POST["kpi_cd"]."',".$_POST["year"].",'".$_POST["quater"]."','".$_POST["prov_cd"]."',".$_POST["qty"].",".$_POST["pct"].",".$_POST["budget"];
+    $sqltext = $sqltext.",".$inputvalues.")";
+
+    if (!$conn->query($sqltext)) {
+        echo "$sqltext<br>";
+        die( "Error: Failed to insert data into table '$mysql_table' ".$conn->error."<br>");
+    }
+}
+
+?>
+<html>
+	<head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link rel="stylesheet" href="../styles/style.css">
+	</head>
+	<body>
+            <h5>Program Details</h5>
+            <form name="PrgDetailsForm" method="post" action="<?php echo basename(__FILE__); ?>" enctype="multipart/form-data" id="Form1">
+<?php
+$sql = "SELECT PROV_CODE, DESCRIPTION FROM PROVINCES";
+$result = $conn->query($sql);
+if (!$conn->query($sql)) {
+    die( "Error: Failed to return data from table PROVINCES ".$conn->error."<br>");
+}
+
+    echo '<select name="prov_cd">';
+    echo '  <option value="none">--Select Province--</option>';
+    if ($result->num_rows > 0) {
+	$rowsremaining = $result->num_rows;
+        $row = $result->fetch_assoc();
+        
+        $result = $conn->query($sql);
+        while($row = $result->fetch_assoc()) {
+            echo '<option value="'.$row["PROV_CODE"].'"> '.$row["DESCRIPTION"].'</option>';
+        }
+    }
+    echo '</select>';
+    echo '<br><br>';
+
+$sql = "SELECT STAT_TYPE, DESCRIPTION FROM STAT_TYPES";
+$result = $conn->query($sql);
+if (!$conn->query($sql)) {
+    die( "Error: Failed to return data from table STAT_TYPES ".$conn->error."<br>");
+}
+
+    echo '<select name="kpi_cd">';
+    echo '  <option value="none">--Select KPI--</option>';
+    if ($result->num_rows > 0) {
+	$rowsremaining = $result->num_rows;
+        $row = $result->fetch_assoc();
+        
+        $result = $conn->query($sql);
+        while($row = $result->fetch_assoc()) {
+            echo '<option value="'.$row["STAT_TYPE"].'"> '.$row["DESCRIPTION"].'</option>';
+        }
+    }
+    echo '</select>';
+    echo '<br><br>';
+
+?>
+                <input type="number" id="year" name="year" value="" placeholder="Year"><br><br>
+                <input type="number" id="qty" name="qty" value="" placeholder="Quantity"><br><br>
+                <input type="number" id="pct" name="pct" value="" placeholder="Qty Percentage"><br><br>
+                <input type="number" id="budget" name="budget" value="" placeholder="Budget"><br><br>
+                <select name="quater">
+                    <option value="none">--Select Quarter--</option>
+                    <option value="1">Quarter 1</option>
+                    <option value="2">Quarter 2</option>
+                    <option value="3">Quarter 3</option>
+                    <option value="4">Quarter 4</option>
+                </select>
+                <br><br>
+                <input type="reset" id="ResetBtn" name="" value="Reset"><br><br>
+                <input type="submit" id="SubmitBtn" name="" value="Submit">
+            </form>
+        </body>
+</html>
